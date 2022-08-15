3b63771e527b0f3344a804bf6458b998213a31a1
Ilya Gavrilov <llxodz@gmail.com>
2022-08-15 16:24:07 +0300

    Добавлен UI к ячейке с настройками игры

---
+++ SpyGameiOS/Screens/Main/MainViewController.swift
@@ -0,0 +1,91 @@
+//
+//  MainViewController.swift
+//  SpyGameiOS
+//
+//  Created by Andrey Firsenko on 14.08.2022.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: BaseViewController {
+    
+    // Private property
+    private var models = [
+        SettingsTableViewCell.Model(id: 0, title: "Игроки"),
+        SettingsTableViewCell.Model(id: 1, title: "Шпионы"),
+        SettingsTableViewCell.Model(id: 2, title: "Таймер"),
+    ]
+    
+    // UI
+    private lazy var tableView = UITableView()
+    
+    // MARK: - Init
+    
+    override init() {
+        super.init()
+        addViews()
+        configureLayout()
+        configureTableView()
+        configureAppearance()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+    
+    // MARK: - Private
+    
+    private func addViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func configureLayout() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func configureAppearance() {
+        
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 0.001, height: 0))
+        tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0.001, height: 0))
+        tableView.register(
+            SettingsTableViewCell.self,
+            forCellReuseIdentifier: SettingsTableViewCell.identifier
+        )
+    }
+}
+
+// MARK: - Extensions UITableView
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: SettingsTableViewCell.identifier,
+            for: indexPath
+        ) as? SettingsTableViewCell else { return UITableViewCell() }
+        
+        cell.configure(with: models[indexPath.row])
+        
+        return cell
+    }
+}
