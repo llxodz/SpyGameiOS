ed8e4db2c74b700849882a97bf636d7008ad2702
Ilya Gavrilov <llxodz@gmail.com>
2023-01-08 21:35:23 +0300

    Добавил, запрещающую регулировать количество игроков в определенных ситуациях

--- SpyGameiOS/Screens/SettingsGame/ViewModel/SettingsGameViewModel.swift
+++ SpyGameiOS/Screens/SettingsGame/ViewModel/SettingsGameViewModel.swift
@@ -9,34 +9,80 @@ import Foundation
 
 final class SettingsGameViewModel {
     
-    private var data: SettingsTableViewCell.Model
+    // Private property
+    private var data: [SettingsTableViewCell.Model]
+    private var position: Int
     
-    init(data: SettingsTableViewCell.Model) {
+    // MARK: - Init
+    
+    init(data: [SettingsTableViewCell.Model], position: Int) {
         self.data = data
+        self.position = position
     }
     
+    // MARK: - Public functions
+    
     func getData() -> SettingsTableViewCell.Model {
-        return data
+        return data[position]
     }
     
     func getCountText() -> String {
-        switch data.fieldType {
-            case .number: return String(data.countText)
-            case .timer: return "\(data.countText) \(L10n.SettingsCell.minute)"
+        switch data[position].fieldType {
+            case .number: return String(data[position].countText)
+            case .timer: return "\(data[position].countText) \(L10n.SettingsCell.minute)"
         }
     }
     
     func plusButtonTapped(complition: @escaping (String) -> Void) {
-        if !(data.countText == data.maxValue) {
-            data.countText += 1
-            complition(getCountText())
+        var role = SettingsCellType(rawValue: position)
+        
+        switch role! {
+        case .players:
+            if (data[position].countText - 2 == data[1].countText) {
+                data[position].countText += 1
+                complition(getCountText())
+            } else if (data[self.position].countText == data[self.position].maxValue) {
+                complition(getCountText())
+            } else {
+                data[position].countText += 1
+                complition(getCountText())
+            }
+        case .spies:
+            if (data[position].countText + 2 == data[0].countText) {
+                complition(getCountText())
+            } else if (data[self.position].countText == data[self.position].maxValue) {
+                complition(getCountText())
+            } else {
+                data[position].countText += 1
+                complition(getCountText())
+            }
+        case .timer:
+            if data[position].countText != data[position].maxValue {
+                data[position].countText += 1
+                complition(getCountText())
+            }
         }
     }
     
     func minusButtonTapped(complition: @escaping (String) -> Void) {
-        if !(data.countText == data.minValue) {
-            data.countText -= 1
-            complition(getCountText())
+        guard let role = SettingsCellType(rawValue: position) else { return }
+        
+        switch role {
+        case .players:
+            if data[position].countText - 2 != data[1].countText && data[self.position].countText != data[self.position].minValue {
+                data[position].countText -= 1
+                complition(getCountText())
+            }
+        case .spies:
+            if data[self.position].countText != data[self.position].minValue {
+                data[position].countText -= 1
+                complition(getCountText())
+            }
+        case .timer:
+            if data[position].countText != data[position].minValue {
+                data[position].countText -= 1
+                complition(getCountText())
+            }
         }
     }
 }
