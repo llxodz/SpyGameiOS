b7299fba2a35b593a67304713ef725be04c3da2a
Andrew Firsenko <andrewfirsenko@gmail.com>
2023-03-26 17:17:52 +0300

    Добавил координатор

--- SpyGameiOS/SceneDelegate.swift
+++ SpyGameiOS/SceneDelegate.swift
@@ -10,14 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        let vc = MainViewController()
-        self.window?.rootViewController = UINavigationController(rootViewController: vc)
+        appCoordinator = AppCoordinator(navigationController: UINavigationController())
+        appCoordinator?.start()
+        self.window?.rootViewController = appCoordinator?.navigationController
         self.window?.makeKeyAndVisible()
     }
 
